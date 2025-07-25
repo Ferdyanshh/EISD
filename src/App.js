e18abdf7f9077ee5f42c416006137ae9a63e6223
Ferdyanshh <fraveny0506@gmail.com>
2025-07-25 23:25:43 +0700

    y

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+// src/App.js
+import React, { useEffect, useState } from 'react';
+import ProductList from './components1/ProductList';
 import './App.css';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(data => setProducts(data))
+      .catch(err => console.error('Error:', err));
+  }, []);
+  const processedProducts = products
+    .map(product => ({
+      ...product,
+      price: product.price * 0.2,
+    }))
+    .filter(product => product.rating.rate > 4.0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>FakeStore Products</h1>
+      <ProductList products={processedProducts} />
     </div>
   );
 }
